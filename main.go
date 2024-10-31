7d2578ff3e15dcd913ad7b1ace3f917104c87dc8
unknown <aryasatyapark@gmail.com>
2024-10-31 14:58:53 +0700

    Menit 50

--- main.go
+++ main.go
@@ -4,12 +4,28 @@ import "fmt"
 
 func main() {
 
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets int = 50
 
-	fmt.Println("Welcome to our", conferenceName, "booking application")
-	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceName, remainingTickets, conferenceName)
+
+	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+	// ask user their name
+	// fmt.Scan(&userName)
+
+	// variable goes to memory address
+	// fmt.Println(remainingTickets)
+	// Example using pointer :
+	// fmt.Println(&remainingTickets)
+	userName = "Tom"
+	userTickets = 2
+
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 
 }
